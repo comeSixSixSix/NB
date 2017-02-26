d5fae22cd1f32688f591a56df117f221e114a38b
comeSixSixSix <13613302851@163.com>
2017-02-26 22:05:03 +0800

    shoucitijao

---
+++ Prefect/Prefect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Prefect
+//
+//  Created by 赵鹏云 on 17/2/26.
+//  Copyright © 2017年 Zpy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Prefect/Prefect/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  Prefect
+//
+//  Created by 赵鹏云 on 17/2/26.
+//  Copyright © 2017年 Zpy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *  btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(100, 100, 100, 30);
+    [btn setTitle:@"按钮" forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor blueColor] forState:UIControlStateNormal] ;
+//    [btn addTarget:self action:@selector(clickOne) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
